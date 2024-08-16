fd70ea8355710a3ac95631ad5efe35a0df8dedbc
Joachim Løvgaard <joachim@loevgaard.dk>
2024-08-16 15:17:39 +0200

    Fix some deps and Psalm errors

--- tests/cronjobs/jobs.php
+++ tests/cronjobs/jobs.php
@@ -7,7 +7,7 @@ use Setono\CronBuilder\CronJob;
 return static function (array $context): iterable {
     yield new CronJob('0 0 * * *', '/usr/bin/php %release_path%/send-report.php', 'Run every day at midnight');
 
-    if ($context['env'] === 'prod') {
+    if ($context['env'] ?? '' === 'prod') {
         yield new CronJob('0 0 * * *', '/usr/bin/php %release_path%/process.php');
     }
 };
