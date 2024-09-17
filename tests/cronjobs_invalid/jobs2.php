b14b182cc92d69ae8438975025c0d0c81f34c458
Joachim Løvgaard <joachim@loevgaard.dk>
2024-09-17 14:02:08 +0200

    Fix code style

--- tests/cronjobs_invalid/jobs2.php
+++ tests/cronjobs_invalid/jobs2.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Setono\CronBuilder\Context;
-use Setono\CronBuilder\CronJob;
 
 return static function (Context $context): string {
     return 'not an iterable';
--- tests/cronjobs_invalid/jobs3.php
+++ tests/cronjobs_invalid/jobs3.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Setono\CronBuilder\Context;
-use Setono\CronBuilder\CronJob;
 
 return static function (Context $context): iterable {
     yield 'not an object';
