c6c22a3cd98317a9fab51ddfd5d8042d4733d014
Joachim Løvgaard <joachim@loevgaard.dk>
2024-09-17 13:21:43 +0200

    Add another variable to the output

--- tests/cronjobs/jobs.php
+++ tests/cronjobs/jobs.php
@@ -6,9 +6,9 @@ use Setono\CronBuilder\Context;
 use Setono\CronBuilder\CronJob;
 
 return static function (Context $context): iterable {
-    yield new CronJob('0 0 * * *', '/usr/bin/php {{ release_path }}/send-report.php {{ args|join(" ") }}', 'Run every day at midnight');
+    yield new CronJob('0 0 * * *', '/usr/bin/php {{ release_path }}/{{ release_number }}/send-report.php {{ args|join(" ") }}', 'Run every day at midnight');
 
     if ($context->get('env') === 'prod') {
-        yield new CronJob('0 0 * * *', '/usr/bin/php {{ release_path }}/process.php {{ args|join(" ") }}');
+        yield new CronJob('0 0 * * *', '/usr/bin/php {{ release_path }}/{{ release_number }}/process.php {{ args|join(" ") }}');
     }
 };
