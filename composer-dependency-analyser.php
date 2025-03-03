1bf528329cdf0333e2e7dbba450f9c8ee65aaaff
Joachim Løvgaard <joachim@loevgaard.dk>
2025-03-03 13:40:45 +0100

    Use shipmonks composer dependency analyser

---
+++ composer-dependency-analyser.php
@@ -0,0 +1,8 @@
+<?php
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+return (new Configuration())
+    ->addPathToExclude(__DIR__ . '/tests')
+;
